559c1e999d47e0dab7f4829475aca163d0acff46
RoyOkite <royokite@gmail.com>
2022-10-24 00:33:39 +0300

    Code Along Complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
+    def name=(value)
+        @this_dogs_name = value
+    end
 
+    def name
+        @this_dogs_name
+    end 
 end
+
+kaido = Dog.new
+kaido.name = "kaido"
+
+puts kaido.name
